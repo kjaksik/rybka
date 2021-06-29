d5668ed31ac276b2b6f7f29693ed1c19d026d2f4
kjaksik <kjaksik@swissblock.net>
2021-06-30 00:24:08 +0200

    Add registration

--- rybka/users/views.py
+++ rybka/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.forms import forms
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Dodano konto dla {username}')
+            return redirect('rybka-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html',{'form':form})
